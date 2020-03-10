583657a7644637e740cdfa48da4a4e2c14929bce
Sergei Glikstein <serge.gl@gmail.com>
2020-03-09 21:50:39 -0400

    Updates

---
+++ precache-manifest.6b795b7945e30a10115d3e89dea2e06e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efddbfa1c713df206603c46f6040583",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7e7e0c56f6c41f7a0798",
+    "url": "/monsters-rolodex/static/css/main.f7aecf9e.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7e0c56f6c41f7a0798",
+    "url": "/monsters-rolodex/static/js/main.1f21d3ed.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
